b586e8869b1cb59b42baa895580ac94ba5df66a3
rudar soni <rudar9647@gmail.com>
2026-06-01 10:21:56 +0530

    refactor: update counter logic to use batch updates and change initial state

--- 04 advance state change/src/App.jsx
+++ 04 advance state change/src/App.jsx
@@ -1,29 +1,65 @@
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [counter, setcounter] = useState(0);
+
+//   function increasenum() {
+//     setcounter(counter + 1);
+//   }
+//   function decreasenum() {
+//     if (counter > 0) {
+//       setcounter(counter - 1);
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <h1>{counter}</h1>
+//       <button onClick={increasenum} className="gap-5 m-5">
+//         increase
+//       </button>
+//       <button onClick={decreasenum}>dencrease</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
 import React, { useState } from "react";
 
 const App = () => {
-  const [counter, setcounter] = useState(0);
+  const [num, setNum] = useState(10);
 
-  function increasenum() {
-    setcounter(counter + 1);
-  }
-  function decreasenum() {
-    if (counter > 0) {
-      setcounter(counter - 1);
-    }
-  }
+  function btnclick() {
 
+    // setNum(num+1)
+    // setNum(num+1)
+    // setNum(num+1)
+
+//Batch update
+    setNum(prev=>(prev+1))
+    setNum(prev=>(prev+1))
+    setNum(prev=>(prev+1))
+
+
+  }
+ 
   return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={increasenum} className="gap-5 m-5">
-        increase
-      </button>
-      <button onClick={decreasenum}>dencrease</button>
+     <h1>{num}</h1>
+     <button onClick={btnclick}>click</button>
     </div>
   );
 };
 
 export default App;
+
+
 
 
 
